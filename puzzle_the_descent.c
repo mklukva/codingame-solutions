053ab2dc29df873dbeeafc086faee337ed5889b0
Mikhail Klukva <kliukin.misha@yandex.ru>
2024-09-04 19:39:38 +0500

    add discription

--- puzzle_the_descent.c
+++ puzzle_the_descent.c
@@ -1,3 +1,14 @@
+/*Your program must destroy the enemy ships by shooting the closest enemy on each turn.
+Rules
+On each start of turn (within the game loop), you obtain information on the two closest enemies:
+
+-enemy1 and dist1: the name and the distance to enemy 1.
+-enemy2 and dist2: the name and the distance to enemy 2.
+
+Before your turn is over (end of the loop), output the value of either enemy1 or enemy2 to shoot the closest enemy.
+*/
+
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,12 +34,6 @@ int main() {
     int dist_2;
     scanf("%d", &dist_2);
     getchar(); 
-
-    // Write an action using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    // You have to output a correct ship name to shoot ("Buzz", enemy1, enemy2,
-    // ...)
     if (dist_1 < dist_2)
         printf("%s\n", &enemy_1); 
     else
